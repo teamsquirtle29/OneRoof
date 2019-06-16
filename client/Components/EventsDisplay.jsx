6012505aabe735bfdf3a7ea59b7f54d8cb553ae2
Victoria Adnet <victoria.adnet@gmail.com>
2019-06-15 22:23:52 -0700

    events flow set up --> NOT tested

--- client/Components/EventsDisplay.jsx
+++ client/Components/EventsDisplay.jsx
@@ -1,8 +1,16 @@
 import React, {Component} from 'react';
 
 function EventsDisplay(props) {
+    let display =[];
+    let eventsList = this.props.eventsList;
+    eventsList.forEach(element =>{
+        display.push(<li>{element}</li>);
+    })
     return (
       <div>
+          <ul>
+            {display}
+          </ul>
       </div>
     );
 }
