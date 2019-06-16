3362ad24dc7d35076de91b8f9512252f812783b4
Brian Kwok <briankwok@brians-mbp.lan>
2019-06-16 14:09:55 -0700

    tenantShell

---
+++ client/Components/tenantEvents.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { NativeRouter } from "react-router-native";
+import { BrowserRouter } from "react-router-dom";
+
+
+class tenantEvents extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  componentDidMount() {
+    fetch()
+    method: 'get'
+    // userId
+  }
+
+  render() {
+  return (
+    <div>
+
+    </div>
+  )
+  }
+}
+
+export default tenantEvents;
--- client/Containers/TenantContainer.jsx
+++ client/Containers/TenantContainer.jsx
@@ -1,15 +1,42 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+// import from child components...
+import * as userActions from '../Actions/userActions.js';
+import * as manActions from '../Actions/manActions.js';
+import ManContainer from './ManContainer.jsx';
+import TenantContainer from './TenantContainer.jsx';
+import { bindActionCreators } from 'redux';
+import tenantPayments from '../Components/tenantPayments';
+import tenantEvents from '../Components/tenantEvents';
 
-class TenantContainer extends Component {
+
+const mapStateToProps = store => ({
+
+})
+
+const mapDispatchToProps = dispatch => {
+  // return an object
+  return {
+
+  }
+}
+
+class tenantContainer extends Component {
   constructor(props) {
-    super(props);
+    super(props)
   }
 
   render() {
     return (
-      <div></div>
-    );
+      <div>
+        <nav>
+          <link to="/Payments" component={tenantPayments}></link>
+          <link to="/Event" component={tenantEvents}></link>
+        </nav>
+      </div>
+    )
   }
+
 }
 
-export default TenantContainer;
+export default connect(mapStateToProps, mapDispatchToProps)(tenantContainer);
