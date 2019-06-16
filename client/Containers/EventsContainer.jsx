af9ae1b368db622d3c22976a7ca4438b165b2ee9
Victoria Adnet <victoria.adnet@gmail.com>
2019-06-15 20:45:26 -0700

    Creating events file

---
+++ client/Containers/EventsContainer.jsx
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import EventCreator from './../Components/EventCreator.jsx';
+import EventsDisplay from './../Components/EventsDisplay.jsx';
+
+class MessageContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+  }
+
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default EventsContainer;
